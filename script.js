753544950f25b896433447a54806df7f74fcd59d
Vinicius Mendite <vinicius.miff@gmail.com>
2020-09-29 23:24:47 -0300

    exibindo informaçoes do candidato referente ao numero digitado

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let sideview = document.querySelector('.split-1-right');
 let numbers = document.querySelector('.split-1-3');
 
 let currentStep = 0;
+let numberCandidate = '';
 
 function startStep() {
   let step = etapas[currentStep];
@@ -13,19 +14,65 @@ function startStep() {
   let numberHtml = '';
 
   for(let i = 0; i < step.numeros; i++) {
-    numberHtml += '<div class="number"></div>';
+    if (i === 0) {
+      numberHtml += '<div class="number focus"></div>';
+    } else { 
+      numberHtml += '<div class="number"></div>';
+    }
   }
 
   yourVoteFor.style.display = 'none';
   role.innerHTML = step.titulo;
-  description.style.display = 'none';
+  description.innerHTML = '';
   warning.style.display = 'none';
   sideview.innerHTML = '';
   numbers.innerHTML = numberHtml;
 }
 
-function clickNumberButton(number) {
-  alert('clicou em' + number)
+function updateInterface() {
+  let step = etapas[currentStep];
+
+  let candidate = step.candidatos.filter((item) => {
+    if (item.numero === numberCandidate) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  if (candidate.length > 0) {
+    candidate = candidate[0];
+
+    yourVoteFor.style.display = 'block';
+    warning.style.display = 'block';
+    description.innerHTML = `Nome: ${candidate.nome}<br/>Partido: ${candidate.partido}`;
+
+    let photosHtml = '';
+    for (i in candidate.fotos) {
+      photosHtml += `          
+      <div class="split-1-image">
+        <img src="images/${candidate.fotos[i].url}" alt="candidato">
+        ${candidate.fotos[i].legenda}
+      </div>`
+    }
+
+    sideview.innerHTML = photosHtml;
+  }
+}
+
+function clickNumberButton(n) {
+  let elNumber = document.querySelector('.number.focus');
+  if (elNumber !== null) {
+    elNumber.innerHTML = n;
+    numberCandidate = `${numberCandidate}${n}`;
+
+    elNumber.classList.remove('focus');
+    if (elNumber.nextElementSibling !== null) {
+      elNumber.nextElementSibling.classList.add('focus');
+    } else {
+      updateInterface();
+    }
+  }
 }
 
 function voteWhite() {
